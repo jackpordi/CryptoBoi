bc8b91891546daf99f4cc738bc4c556326ab6219
Jack Pordi <jackel119@gmail.com>
2017-11-15 02:18:18 +0000

    Relatively familiar with the API now

--- bittrex/main.py
+++ bittrex/main.py
@@ -20,21 +20,28 @@ class Bot(object):
         while True:
             for coin in Currencies:
                 self.print_currency(coin)
+        return
 
     def print_currency(self, coin):
         coin_balance = self.btrx.get_balance(coin.value)
-        print("Current ", coin.value, "Balance = ",format(coin_balance['result']['Balance'], 'f').rstrip('0'), " perBTC price = ", self.get_market_price(coin))
+        market = 'BTC-' + coin.value + ' price ='
+        output = line_new = '{:>6} {:>4} {:>10}{:>12} {:>12} {:>16}'.format("Current", coin.value, "Balance = ",float(coin_balance['result']['Balance']), market, self.get_market_price(coin))
+        print(output)
+        #print("Current", coin.value, "Balance = ",float(coin_balance['result']['Balance']), "perBTC price = ", self.get_market_price(coin))
     
     def get_market_price(self, coin):
         if coin.value == "BTC":
             return "1.0000"
         else:
-            return (self.btrx.get_marketsummary('BTC-' + coin.value)['result']['Last'])
+            while True:
+                marketData = self.btrx.get_marketsummary('BTC-' + coin.value)
+                if marketData['result']['MarketName'] == 'BTC-' + coin.value:
+                    return marketData['result']['Last']
+                #print('failed, exchange returned ', marketData['result']['MarketName'])
 
 
 if __name__ == '__main__' :
     bot = Bot()
-    #bot.runbot()
-    print((bot.btrx.get_marketsummary('BTC-ETH')))
-    print(bot.get_market_price(Currencies.ETH))
+    bot.runbot()
+    #print(bot.get_market_price(Currencies.ETH))
     exit()
